f130f9069598baa6f9154accdad0ace450cca9a0
NikSan <nikulinaleksandr@gmail.com>
2015-01-09 22:03:28 +0300

    fix

--- app/models/russian_invoices/contract.rb
+++ app/models/russian_invoices/contract.rb
@@ -1,6 +1,6 @@
 class RussianInvoices::Contract < RussianInvoices::BaseModel
 
-  attr_accessor :number, :from_date, :name
+  attr_accessor :number, :from_date, :name,
                 :customer_name, :customer_details, :customer_signature, :customer_print,
                 :performer_name, :performer_details, :performer_signature, :performer_print
 

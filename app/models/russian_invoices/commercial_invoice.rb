d14bffe91f5446d0aa4b4b2db203dbddf6543348
NikSan <nikulinaleksandr@gmail.com>
2015-01-11 22:34:14 +0300

    upd

--- app/models/russian_invoices/commercial_invoice.rb
+++ app/models/russian_invoices/commercial_invoice.rb
@@ -1,8 +1,20 @@
 class RussianInvoices::CommercialInvoice < RussianInvoices::BaseModel
 
-  attr_accessor :invoice_number, :invoice_from_date, :correction_number, :correction_date
+  attr_accessor :invoice_number, :invoice_from_date, :correction_number, :correction_date,
+                :vendor_name, :vendor_address, :vendor_inn, :vendor_kpp,
+                :shipper_name, :shipper_address,
+                :consignee_name, :consignee_address, :to_the_payment_documents,
+                :buyer_name, :buyer_address, :buyer_inn, :buyer_kpp, :goods,
+                :chief_organization, :chief_accountant,
+                :chief_organization_signature, :chief_accountant_signature,
+                :stamp
 
-  validates_presence_of :invoice_number, :invoice_from_date
+  validates_presence_of :invoice_number, :invoice_from_date, :correction_number,
+                        :correction_date,
+                        :vendor_name, :vendor_address, :vendor_inn, :vendor_kpp,
+                        :shipper_name, :shipper_address,
+                        :consignee_name, :consignee_address, :to_the_payment_documents,
+                        :buyer_name, :buyer_address, :buyer_inn, :buyer_kpp
 
   before_save :prepare_data
 

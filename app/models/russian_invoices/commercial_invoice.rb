3080f8803217610829533da439836aa4f9b13b96
NikSan <nikulinaleksandr@gmail.com>
2015-01-11 20:25:19 +0300

    upd

--- app/models/russian_invoices/commercial_invoice.rb
+++ app/models/russian_invoices/commercial_invoice.rb
@@ -14,6 +14,12 @@ class RussianInvoices::CommercialInvoice < RussianInvoices::BaseModel
 
     def prepare_data
       self.invoice_number = invoice_number.to_s.rjust(6, '0')
+      self.correction_number = if correction_number
+        invoice_number.to_s.rjust(3, '0')
+      else
+        '---'
+      end
+      self.correction_date ||= '---'
     end
 
 end

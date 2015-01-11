264f0aeffbfe860ca426c7b4638a01a9ce0c2c2d
NikSan <nikulinaleksandr@gmail.com>
2015-01-11 14:30:42 +0300

    certeficete_of_completion id complete =)

--- app/models/russian_invoices/certificate_of_completion.rb
+++ app/models/russian_invoices/certificate_of_completion.rb
@@ -3,11 +3,15 @@ class RussianInvoices::CertificateOfCompletion < RussianInvoices::BaseModel
   attr_accessor :number, :from_date,
                 :customer_name, :customer_inn, :customer_kpp, :customer_address,
                 :performer_name, :performer_inn, :performer_kpp, :performer_address,
-                :goods, :summ, :nds, :total_summ, :performer_signature, :performer_stamp
+                :goods, :summ, :nds, :performer_signature, :performer_stamp
 
-  validates :total_summ, presence: true, numericality: true
+  validates :nds, presence: true, numericality: true
   validates_presence_of :number, :from_date,
                         :customer_name, :customer_inn, :customer_kpp, :customer_address,
                         :performer_name, :performer_inn, :performer_kpp, :performer_address
+
+  def total_summ
+    goods.sum{ |good| good[:quantity]*good[:price] }.round(2)
+  end
 
 end

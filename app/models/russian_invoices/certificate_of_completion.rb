86387789f4247255b8f4eeee703dcb47d736ea68
NikSan <nikulinaleksandr@gmail.com>
2015-01-11 13:15:19 +0300

    upd

--- app/models/russian_invoices/certificate_of_completion.rb
+++ app/models/russian_invoices/certificate_of_completion.rb
@@ -3,7 +3,7 @@ class RussianInvoices::CertificateOfCompletion < RussianInvoices::BaseModel
   attr_accessor :number, :from_date,
                 :customer_name, :customer_inn, :customer_kpp, :customer_address,
                 :performer_name, :performer_inn, :performer_kpp, :performer_address,
-                :goods, :summ, :nds, :total_summ, :performer_signature
+                :goods, :summ, :nds, :total_summ, :performer_signature, :performer_stamp
 
   validates :total_summ, presence: true, numericality: true
   validates_presence_of :number, :from_date,
